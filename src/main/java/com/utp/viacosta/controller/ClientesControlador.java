0fd349a57f864f57b730e334f436b48800292ee9
Andree Bermudez <andreebermudez1408@gmail.com>
2024-10-24 16:03:43 -0500

    Cambios no relevantes

--- src/main/java/com/utp/viacosta/controller/ClientesControlador.java
+++ src/main/java/com/utp/viacosta/controller/ClientesControlador.java
@@ -2,6 +2,7 @@ package com.utp.viacosta.controller;
 
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class ClientesControlador {
 }
--- src/main/java/com/utp/viacosta/dao/EmpleadoRepository.java
+++ src/main/java/com/utp/viacosta/dao/EmpleadoRepository.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmpleadoRepository extends JpaRepository<EmpleadoModel, Integer> {
+
     EmpleadoModel findByCorreo(String usuario);
 }
